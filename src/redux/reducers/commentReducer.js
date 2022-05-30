09b4e712778f86d951276fed6e5fb841d6b0cfab
Volodymyr <vavemen29@gmail.com>
2022-05-31 02:53:43 +0300

    addComments

--- src/redux/reducers/commentReducer.js
+++ src/redux/reducers/commentReducer.js
@@ -1,16 +1,13 @@
 import { actions } from "./actions";
 
 const initialState = {
-    comments:[],
-}
+  comments: []
+};
 
 function commentReducer(state = initialState, action) {
   switch (action.type) {
     case actions.SET_COMMENTS:
-      return { ...state,
-        comments:action.payload
-   
-    };
+      return { ...state, comments: action.payload };
     default:
       return state;
   }
